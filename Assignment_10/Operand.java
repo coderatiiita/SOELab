6fdbc2ea55865ffccc42dce8d1eeeb981dfcc3bc
Shubham Vashishtha <ihm2014007@iiita.ac.in>
2016-11-15 19:55:29 +0530

    Add files via upload
    Utility Classes

---
+++ Assignment_10/Operand.java
@@ -0,0 +1,14 @@
+package myAssignment10;
+
+/** @author shubhvash */
+
+public class Operand {
+    String name;
+    int count;
+
+    public Operand(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+    
+}
---
+++ Assignment_10/Operator.java
@@ -0,0 +1,13 @@
+package myAssignment10;
+
+/** @author shubhvash */
+
+public class Operator {
+    String name;
+    int count;
+
+    public Operator(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
